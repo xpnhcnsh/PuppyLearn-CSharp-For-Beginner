d989c7a79e64caefc1b82b49259e4a79b9f1b88a
xpnhcnsh <xpnhcnsh@gmail.com>
2024-07-27 21:57:55 +0800

    add Ch1_l2

--- Checked/Program.cs
+++ Checked/Program.cs
@@ -4,68 +4,75 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
 
-            #region 变量
-            string variable1 = "This is variable1.";
-            string variable2 = "This is variable2, which is just behind variable1";
-            Console.WriteLine(variable1);
-            #endregion
+            //#region 变量
+            //string variable1 = "This is variable2";
+            //string variable2 = "This is variable2, which is just behind variable1.";
+            //Console.WriteLine(variable2);
+            //#endregion
 
-            #region 字符串
-            Console.WriteLine(variable1 + variable2);
-            //字符串差值 string interpolation
-            Console.WriteLine($"{variable1}{variable2}");
-            #endregion
+            //#region 字符串
+            //string variable3 = ", which is just behind variable1.";
+            //Console.WriteLine(variable1 + variable3);
+            ////字符串差值 string interpolation
+            //Console.WriteLine($"Hello world! {variable1}--{variable3}");
+            //#endregion
 
-            //#region char
-            //char a = 'a';
+            //#region character 字符
+            //char a = ',';
             //Console.WriteLine(a);
             //#endregion
 
-            //#region int
+            #region int  integer 整型  
             //int a = 1;
             //int b = 2;
             //int c = a + b;
+            //string d = "1";
+            //string e = "2";
+            //string f = d + e;
             //Console.WriteLine(c);
-            //#endregion
+            //Console.WriteLine(f);
+            #endregion
 
             //#region float(32)/double(64)/decimal(128)
-            //float a = 0.1f;
+            //int a = 1;
             //float b = 2.2f;
             //float c = a + b;
             //Console.WriteLine(c);
             //#endregion
 
-            //#region boolean
+            #region boolean 与或非
             //bool a = true;
             //bool b = false;
-            //bool c = a&b;
-            //bool d = a|b;
-            //bool e = !a;
-            //#endregion
+            //bool c = a & b; //与
+            //Console.WriteLine(c);
+            //bool d = a | b; //或
+            //Console.WriteLine(d);
+            //bool e = !a; //非
+            //Console.WriteLine(e);
+            #endregion
 
-            //#region switch two values
+            #region switch two values
             //int a = 10;
             //int b = 20;
-            //// switch value for a and b.
+            //// switch value for a and b. temporal临时的 暂时的
             //int temp = a;
             //a = b;
             //b = temp;
             //Console.WriteLine($"a={a};b={b}");
-            //#endregion
+            #endregion
 
-            //#region dynamic动态类型
+            #region dynamic动态类型
             //dynamic a = 10;
             //a = "lala";
             //Console.WriteLine(a);
-            //#endregion
-
-            #region var编译器可自行推断类型
-            var a = "puppylearn";
-            var b = 10.3d;
-            Console.WriteLine($"a: {a.GetType().Name},b: {b.GetType().Name}");
             #endregion
+
+            //#region var编译器可自行推断类型 variable
+            //var a = "puppylearn";
+            //var b = 10.3d;
+            //Console.WriteLine($"a: {a.GetType().Name},b: {b.GetType().Name}");
+            //#endregion
         }
     }
 
