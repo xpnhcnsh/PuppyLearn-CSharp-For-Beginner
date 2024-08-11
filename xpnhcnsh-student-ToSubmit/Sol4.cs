75e7fc021f38e50ab680052a4c50bdffb19c0385
HanchenNan <xpnhcnsh@gmail.com>
2024-08-11 20:36:06 +0800

    commit for quiz 4

---
+++ xpnhcnsh-student-ToSubmit/Sol4.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace xpnhcnsh_student_ToSubmit
+{
+    public static class Sol4
+    {
+        public static void Run()
+        {
+            Console.WriteLine("this is the answer for quiz4");
+        }
+    }
+}
