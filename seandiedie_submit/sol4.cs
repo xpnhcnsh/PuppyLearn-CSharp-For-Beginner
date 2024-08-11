e6531185c60464930c5eec7a3ae1bef05e2253dc
seandiedie <3173127181@qq.com>
2024-08-11 22:31:19 +0800

    完成第四题

---
+++ seandiedie_submit/sol4.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace seandiedie_submit
+{
+    internal class sol4
+    {
+    }
+}
