70982f5efece4b97c506641de29bdc9c59d9dfc6
xpnhcnsh <xpnhcnsh@gmail.com>
2024-08-20 00:53:32 +0800

    1. update quiz7; 2. add sol5.cs; 3. update Solution/Program.cs

---
+++ Solutions/Sol6.cs
@@ -0,0 +1,32 @@
+﻿namespace Solutions
+{
+    //Quiz6:
+    //给一个List<int>，求前n项和。提示：使用递归。
+    public class Sol6
+    {
+        private dynamic _SumOfFirstNs(List<int> input, int n, out bool flag)
+        {
+            if (n < 0 || n > input.Count)
+            {
+                flag = false;
+                return "n can neither be negative nor exceed length of input list";
+            }
+            if (n == 0)
+            {
+                flag = true;
+                return 0;
+            }
+            else
+            {
+                flag = true;
+                return input[n - 1] + _SumOfFirstNs(input, n - 1, out bool temp);
+            }
+        }
+
+        public bool SumOfFirstNs(List<int> input, int n, out dynamic res)
+        {
+            res = _SumOfFirstNs(input, n, out bool flag);
+            return flag;
+        }
+    }
+}
