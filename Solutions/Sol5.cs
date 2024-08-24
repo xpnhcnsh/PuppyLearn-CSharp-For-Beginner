0bbdc52e16cee500604989dee805fff2a299066a
xpnhcnsh <xpnhcnsh@gmail.com>
2024-08-24 17:35:01 +0800

    sync sol5,6,7

---
+++ Solutions/Sol5.cs
@@ -0,0 +1,36 @@
+﻿namespace Solutions
+{
+    //Quiz5:
+    //读取用户输入的100-999之间的整数，将其个位、十位、百位数值相加，显示其值。
+    public static class Sol5
+    {
+        public static void Run()
+        {
+            while (true)
+            {
+                Console.WriteLine("Please input an integer between 100-999");
+                string inputStr = Console.ReadLine();
+                int input;
+                if (string.IsNullOrEmpty(inputStr))
+                {
+                    Console.WriteLine("Input can not be empty!");
+                    continue;
+                }
+                else if (inputStr.ToLower().Equals("exit"))
+                {
+                    Environment.Exit(0);
+                }
+                if (!int.TryParse(inputStr, out input) || input < 100 || input > 999)
+                {
+                    Console.WriteLine("Input must be an integer between 100-999");
+                    continue;
+                }
+                int hundreds = input/100;
+                int units = input % 10;
+                int tens = (input - units)/10%10;
+                int sum = hundreds + tens + units;
+                Console.WriteLine($"Sum of units, tens and hundreds is: {sum}");
+            }
+        }
+    }
+}
---
+++ Solutions/Sol7.cs
@@ -0,0 +1,103 @@
+﻿namespace Solutions
+{
+    //Quiz7:
+    //反转一个List<T>，用户可以自定义需要反转的范围，例如输入0,2，表示只对index在[1,3]的sublist进行反转，其余部分不变；如果只
+    //输入一个List<T>，那么将所有内容反转。
+    public static class Sol7
+    {
+        /// <summary>
+        /// Use in the Linq way, achieved by List extension method (Note to use extension, the class must be static).
+        /// Reverse the whole input list.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="input">Input list.</param>
+        /// <returns>Return a new list.</returns>
+        public static List<T> ReverseAListLinq<T>(this List<T> input)
+        {
+            return _ReverseAList(input, 0, input.Count - 1);
+        }
+        /// <summary>
+        /// Use in the static function way. Reverse the whole input list.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="input">Input list.</param>
+        /// <returns>Return a new list.</returns>
+        public static List<T> ReverseAList<T>(List<T> input)
+        {
+            return _ReverseAList(input, 0, input.Count - 1);
+        }
+
+        /// <summary>
+        /// Use in the Linq way, achieved by List extension method (Note to use extension, the class must be static).
+        /// Only reverse sublist defined by low and high index.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="input">Input list.</param>
+        /// <param name="low">Low index.</param>
+        /// <param name="high">High index.</param>
+        /// <returns>Return a new reversed list</returns>
+        public static List<T> ReverseAListLinq<T>(this List<T> input, int low, int high)
+        {
+            try
+            {
+                if (low > high)
+                    throw new Exception("Low index can not exceed high index!");
+                else
+                    return _ReverseAList(input, low, high);
+            }
+            catch (Exception)
+            {
+
+                throw; ;
+            }
+        }
+
+        /// <summary>
+        /// Use in the static function way. Only reverse sublist defined by low and high index.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="input">Input list.</param>
+        /// <param name="low">Low index.</param>
+        /// <param name="high">High index.</param>
+        /// <returns>Return a new reversed list</returns>
+        public static List<T> ReverseAList<T>(List<T> input, int low, int high)
+        {
+            try
+            {
+                if (low > high)
+                    throw new Exception("Low index can not exceed high index!");
+                else
+                    return _ReverseAList(input, low, high);
+            }
+            catch (Exception)
+            {
+
+                throw; ;
+            }
+            
+        }
+
+        /// <summary>
+        /// Use recursion to reverse an input list.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="input">Input list.</param>
+        /// <param name="low">Low index.</param>
+        /// <param name="high">High index.</param>
+        /// <returns></returns>
+        private static List<T> _ReverseAList<T>(List<T> input, int low, int high) 
+        {
+            if (low >= high)
+            {
+                return input;
+            }
+            else
+            {
+                T temp = input[low];
+                input[low] = input[high];
+                input[high] = temp;
+                return _ReverseAList(input, low+1, high-1);
+            }
+        }
+    }
+}
